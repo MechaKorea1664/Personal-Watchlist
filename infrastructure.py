b259ff717f8c59ed1efc257b8720e57d65cfe23a
MechaKorea1664 <107726683+MechaKorea1664@users.noreply.github.com>
2022-11-01 01:09:37 -0500

    Home screen WIP
    Home screen random suggestion box is being worked on.

--- infrastructure.py
+++ infrastructure.py
@@ -10,26 +10,62 @@ class infrastructure:
         self.font = t_font
         self.username = u_username
 
-        print(self.res_x,self.res_y)
-    def widget_suggestion(self):
-        frame = Frame(self.master)
-        #canvas_thumb = Canvas(frame,width=100,height=100)
-        #canvas_thumb.pack()
-        #self.thumb = PhotoImage(file='./resources/example.png').subsample(3,3)
-        #canvas_thumb.create_image(10,10,image=self.thumb)
-        self.im1 = Image.open("./resources/example.png").resize((self.res_x/5,self.res_y/5),Image.ANTIALIAS)
-        self.tkimage = ImageTk.PhotoImage(self.im1)
-        Label(frame,image=self.tkimage).grid(column=0,row=0)
-        frame.grid(column=1,row=0)
+    def home_suggestion(self):
+        frame_top = Frame(
+            self.page_main_frame,
+            width=self.res_x-self.navbar_res_x,
+            height=int(self.res_y/5),
+            background='red',
+            border=5,
+            relief=RAISED
+            )
+        frame_top.propagate(False)
+        frame_top.columnconfigure(0,weight=1)
+        frame_top.columnconfigure(1,weight=1)
+        
+        frame_text = Frame(
+            frame_top,
+            background='gray'
+            )
+        frame_text.propagate(False)
+        frame_text.rowconfigure(0,weight=1)
+        frame_text.rowconfigure(1,weight=2)
+        
+        Label(
+            master=frame_text,
+            text='Random suggestion from your playlist!',
+            font=(self.font,12)
+            ).grid(column=0,row=0,sticky=NW)
+        
+        Label(
+            master=frame_text,
+            text='<INSERT TITLE HERE>',
+            font=(self.font,15)
+            ).grid(column=0,row=1,sticky=NW)
+        
+        frame_top.columnconfigure(0,weight=1)
+        img = Image.open('./resources/example.png').resize((100,100),Image.ANTIALIAS)
+        self.thumbnail=ImageTk.PhotoImage(img)
+        Label(master=frame_top,image=self.thumbnail).grid(column=0,row=0)
+        frame_top.grid(column=0,row=0,sticky=W)
+        frame_text.grid(column=1,row=0,sticky=W)
+    
+    def reset_frame(self):
+        for i in self.page_main_frame.winfo_children():
+            i.destroy()
+    
+    def home(self):
+        infrastructure.reset_frame(self)
+        infrastructure.home_suggestion(self)
     
     def window_size(self):
         self.master.update()
         self.res_x = self.master.winfo_width()
         self.res_y = self.master.winfo_height()
     
-    def placeholder(self):
+    def page_frame(self):
         infrastructure.window_size(self)
-        placeholder_Frame = Frame(
+        self.page_main_frame = Frame(
             master=self.master,
             background='gray',
             height=self.res_y,
@@ -37,8 +73,8 @@ class infrastructure:
             relief=RIDGE,
             border=5
             )
-        placeholder_Frame.propagate(True)
-        placeholder_Frame.grid(column=1,row=0)
+        self.page_main_frame.propagate(False)
+        self.page_main_frame.grid(column=1,row=0,sticky=NSEW)
     
     def sidebar(self):
         infrastructure.window_size(self)
@@ -69,26 +105,26 @@ class infrastructure:
         # Buttons for navbar
         Button(
             frame,text='Home',
-            command=lambda: infrastructure.widget_suggestion(self),
-            font=self.font,
+            command=lambda: infrastructure.home(self),
+            font=(self.font,10),
             ).grid(column=0,row=0,sticky=NSEW)
         
         Button(
             frame,
             text='Categories',
-            font=self.font
+            font=(self.font,10)
             ).grid(column=0,row=1,sticky=NSEW)
         
         Button(
             frame,
             text='Other',
-            font=self.font
+            font=(self.font,10)
             ).grid(column=0,row=2,sticky=NSEW)
         
         Button(
             frame,
             text='Settings',
-            font=self.font
+            font=(self.font,10)
             ).grid(column=0,row=3,sticky=NSEW)
         
         # Filler for space below the navbar
@@ -101,4 +137,4 @@ class infrastructure:
             ).grid(column=0,row=1,sticky=NW)
         frame.grid(column=0,row=0)
         main_frame.grid(column=0,row=0)
-        infrastructure.placeholder(self)
+        infrastructure.page_frame(self)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from doctest import master
 from tkinter import *
 from file_manager import file_to_dict
 from infrastructure import *
+
 
 # Dictionary of settings in SETTINGS.txt.
 dict_settings = file_to_dict('SETTINGS.txt')
@@ -12,17 +12,22 @@ window_main = Tk()
 window_main.title(dict_settings['OTHER']['WINDOWTITLE'])
 window_main.geometry(dict_settings['DISPLAY']['WINDOWSIZE'])
 window_main.resizable(dict_settings['DISPLAY']['WINDOWRESIZE'],dict_settings['DISPLAY']['WINDOWRESIZE'])
+window_main.columnconfigure(1,weight=1)
+
+
 # Declaration of variables with user-preferred values defined in SETTINGS.txt
 pref_font = dict_settings['APPEARENCE']['FONT']
 pref_resolution = dict_settings['DISPLAY']['WINDOWSIZE'].split('x') # [x,y]
 pref_username = user_profile['USERINFO']['USERNAME']
 
+
 # defining imported classes
 infra = infrastructure(
     window_main,
-    (pref_font,10),
+    pref_font,
     pref_username
     )
+
 
 # Sidebar, series of buttons.
 infra.sidebar()
