f05a557bd0a7215cce9b52da35e99216da1fae10
MechaKorea1664 <107726683+MechaKorea1664@users.noreply.github.com>
2022-10-31 19:56:02 -0500

    GUI foundation complete
    It starts now!

--- infrastructure.py
+++ infrastructure.py
@@ -3,10 +3,10 @@ from tkinter.font import BOLD
 from PIL import Image, ImageTk
 
 class infrastructure:
-    def __init__(self,t_master,t_resx,t_resy,t_font,u_username):
+    def __init__(self,t_master,t_font,u_username):
         self.master = t_master
-        self.res_x = t_resx
-        self.res_y = t_resy
+        self.res_x = 0
+        self.res_y = 0
         self.font = t_font
         self.username = u_username
 
@@ -22,24 +22,49 @@ class infrastructure:
         Label(frame,image=self.tkimage).grid(column=0,row=0)
         frame.grid(column=1,row=0)
     
+    def window_size(self):
+        self.master.update()
+        self.res_x = self.master.winfo_width()
+        self.res_y = self.master.winfo_height()
+    
+    def placeholder(self):
+        infrastructure.window_size(self)
+        placeholder_Frame = Frame(
+            master=self.master,
+            background='gray',
+            height=self.res_y,
+            width=self.res_x-self.navbar_res_x,
+            relief=RIDGE,
+            border=5
+            )
+        placeholder_Frame.propagate(True)
+        placeholder_Frame.grid(column=1,row=0)
+    
     def sidebar(self):
+        infrastructure.window_size(self)
         # Values for Navbar:
-        self.navbar_res_x = self.res_x/10
-        self.navbar_res_y = self.res_y/10
-        print('navbar',self.navbar_res_x)
+        self.navbar_res_x = int(self.res_x/5)
+        self.navbar_res_y = int(self.res_y/5)
         
         # Sidebar:
+        main_frame = Frame(self.master, width=self.navbar_res_x, height=self.navbar_res_y+10)
+        main_frame.propagate(False)
+        
         frame = Frame(
-            self.master,
+            main_frame,
             relief=SUNKEN,
             width=self.navbar_res_x,
+            height=self.navbar_res_y,
             border=10,
             background='white',
-            height=self.navbar_res_y,
             )
         frame.columnconfigure(0,weight=1)
-        frame.grid_propagate(0)
-        print(self.res_x,self.res_y)
+        frame.rowconfigure(0,weight=1)
+        frame.rowconfigure(1,weight=1)
+        frame.rowconfigure(2,weight=1)
+        frame.rowconfigure(3,weight=1)
+        frame.grid_propagate(False)
+        
         
         # Buttons for navbar
         Button(
@@ -51,8 +76,7 @@ class infrastructure:
         Button(
             frame,
             text='Categories',
-            font=self.font,
-            command=print('Categories clicked!')
+            font=self.font
             ).grid(column=0,row=1,sticky=NSEW)
         
         Button(
@@ -69,10 +93,12 @@ class infrastructure:
         
         # Filler for space below the navbar
         Frame(
-            self.master,
+            main_frame,
             relief=RAISED,
             height=self.res_y-self.navbar_res_y,
-            width=self.navbar_res_x, 
+            width=self.navbar_res_x,
             border=5
-            ).grid(column=0,row=1)
-        return frame
+            ).grid(column=0,row=1,sticky=NW)
+        frame.grid(column=0,row=0)
+        main_frame.grid(column=0,row=0)
+        infrastructure.placeholder(self)
--- main.py
+++ main.py
@@ -12,37 +12,19 @@ window_main = Tk()
 window_main.title(dict_settings['OTHER']['WINDOWTITLE'])
 window_main.geometry(dict_settings['DISPLAY']['WINDOWSIZE'])
 window_main.resizable(dict_settings['DISPLAY']['WINDOWRESIZE'],dict_settings['DISPLAY']['WINDOWRESIZE'])
-
 # Declaration of variables with user-preferred values defined in SETTINGS.txt
 pref_font = dict_settings['APPEARENCE']['FONT']
 pref_resolution = dict_settings['DISPLAY']['WINDOWSIZE'].split('x') # [x,y]
 pref_username = user_profile['USERINFO']['USERNAME']
 
-
-# Creating master_frame
-master_frame = Frame(
-    master=window_main,
-    background='pink',
-    width=int(pref_resolution[0]),
-    height=int(pref_resolution[1])
-    )
-master_frame.rowconfigure(0,weight=1)
-master_frame.columnconfigure(0,weight=1)
-master_frame.grid_propagate(False)
-master_frame.pack(fill="both", expand=True)
-
-print('width',window_main.winfo_width(),' height',window_main.winfo_height())
 # defining imported classes
 infra = infrastructure(
-    master_frame,
-    master_frame.winfo_width(),
-    master_frame.winfo_height(),
+    window_main,
     (pref_font,10),
     pref_username
     )
 
 # Sidebar, series of buttons.
-infra.sidebar().grid(column=0,row=0)
-
-
+infra.sidebar()
+window_main.update()
 mainloop()
